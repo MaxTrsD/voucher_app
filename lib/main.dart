d955180fb7261a0a6281bbbfc1e281296486aca1
MaxTrsD <maxneydomingos@gmail.com>
2023-11-25 01:48:31 -0300

    Melhorias de UI, salvamento e qualidade de imagem.

--- lib/main.dart
+++ lib/main.dart
@@ -1,12 +1,10 @@
 import 'dart:typed_data';
 import 'dart:ui' as ui;
-import 'package:path_provider/path_provider.dart';
-import 'dart:io';
-import 'package:flutter/services.dart' show rootBundle;
 import 'package:flutter/material.dart';
 import 'package:flutter/rendering.dart';
-import 'package:image/image.dart' as img;
+import 'package:get/get.dart';
 import 'package:image_gallery_saver/image_gallery_saver.dart';
+import 'package:permission_handler/permission_handler.dart';
 
 void main() => runApp(MyApp());
 
@@ -28,121 +26,163 @@ class MyHomePage extends StatefulWidget {
 class _MyHomePageState extends State<MyHomePage> {
   final GlobalKey _globalKey = GlobalKey();
 
-  String customerName = '';
-  String paymentInfo = '';
+  String nomesDeClientes = '';
+  String tipoDePasseio = '';
+  String dataDePasseio = '';
+  String dataDeEmissao = '';
+  String referentePasseio = '';
+  String valorPago = '';
+  String valorAReceber = '';
+  String valorTotal = '';
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text('Voucher App'),
+        backgroundColor: Colors.orangeAccent,
+        title: Center(
+          child: Text(
+            'Voucher',
+            style: TextStyle(color: Colors.white),
+          ),
+        ),
       ),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            RepaintBoundary(
-              key: _globalKey,
-              child: Container(
-                width: 350,
-                height: 242,
-                color: ui.Color.fromARGB(192, 158, 158, 158),
-                child: Stack(
-                  fit: StackFit.expand,
-                  children: <Widget>[
-                    // Display the base voucher image
-                    Image.asset('assets/images/voucher (5).png',
-                        width: 300, height: 200),
-                    // Display customer name and payment info
-                    Positioned(top: 90, left: 40, child: Text(customerName)),
-                    Positioned(
-                      top: 90,
-                      left: 300,
-                      child: Text(
-                        paymentInfo,
+      body: Scrollbar(
+        child: SingleChildScrollView(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.start,
+            children: <Widget>[
+              Divider(),
+              RepaintBoundary(
+                key: _globalKey,
+                child: Container(
+                  width: 400,
+                  height: 179,
+                  color: ui.Color.fromARGB(0, 0, 0, 0),
+                  child: Stack(
+                    fit: StackFit.expand,
+                    children: <Widget>[
+                      // Display the base voucher image
+                      Image.asset('assets/images/voucher (5).png',
+                          width: 300, height: 200),
+                      // Display customer name and payment info
+                      Positioned(
+                        top: 90,
+                        left: 40,
+                        child: Text(
+                          nomesDeClientes,
+                          style: TextStyle(
+                              color: const ui.Color.fromARGB(255, 0, 0, 0)),
+                        ),
                       ),
-                    ),
-                  ],
+                      Positioned(
+                        top: 90,
+                        left: 300,
+                        child: Text(
+                          valorPago,
+                          style: TextStyle(
+                              color: const ui.Color.fromARGB(255, 0, 0, 0)),
+                        ),
+                      ),
+                    ],
+                  ),
                 ),
               ),
-            ),
-            SizedBox(height: 20),
-            // TextFields for customer name and payment info
-            TextField(
-              onChanged: (value) {
-                setState(() {
-                  customerName = value;
-                });
-              },
-              decoration: InputDecoration(labelText: 'Nome do Cliente'),
-            ),
-            TextField(
-              onChanged: (value) {
-                setState(() {
-                  paymentInfo = value;
-                });
-              },
-              decoration:
-                  InputDecoration(labelText: 'Informações de Pagamento'),
-            ),
-            SizedBox(height: 20),
-            ElevatedButton(
-              onPressed: _saveImage,
-              child: Text('Salvar Imagem'),
-            ),
-          ],
+              Divider(),
+              SizedBox(height: 20),
+              // TextFields for customer name and payment info
+              TextField(
+                onChanged: (value) {
+                  setState(() {
+                    nomesDeClientes = value;
+                  });
+                },
+                decoration: InputDecoration(labelText: 'Nome do Cliente'),
+              ),
+              TextField(
+                onChanged: (value) {
+                  setState(() {
+                    valorPago = value;
+                  });
+                },
+                decoration:
+                    InputDecoration(labelText: 'Informações de Pagamento'),
+              ),
+              SizedBox(height: 20),
+              Container(
+                height: 50,
+                width: 200,
+                decoration: BoxDecoration(
+                  color: Color.fromARGB(255, 27, 27, 57),
+                  borderRadius: BorderRadius.circular(25),
+                ),
+                child: Center(
+                  child: TextButton(
+                    onPressed: _saveImage,
+                    child: Text(
+                      'Salvar Imagem',
+                      style: TextStyle(color: Colors.white),
+                    ),
+                  ),
+                ),
+              ),
+            ],
+          ),
         ),
       ),
     );
   }
 
   void _saveImage() async {
+    // Verificar se a permissão já foi concedida
+    Permission.storage.request();
+    var status = await Permission.storage.status;
+
+    if (status.isGranted) {
+      // Se a permissão já foi concedida, salvar a imagem
+      _saveImageInternal();
+    } else {
+      // Se a permissão ainda não foi concedida, solicitar permissão
+      if (await Permission.storage.request().isGranted) {
+        // Se o usuário concedeu permissão, salvar a imagem
+        _saveImageInternal();
+      } else {
+        // Se o usuário negou a permissão, exibir uma mensagem ou tratar conforme necessário
+        print('Permissão de armazenamento negada pelo usuário. $status');
+      }
+    }
+  }
+
+  void _saveImageInternal() async {
     RenderRepaintBoundary boundary =
         _globalKey.currentContext!.findRenderObject() as RenderRepaintBoundary;
-    ui.Image image = await boundary.toImage();
+    ui.Image image = await boundary.toImage(pixelRatio: 10.0);
     ByteData? byteData = await image.toByteData(format: ui.ImageByteFormat.png);
 
     if (byteData != null) {
       Uint8List pngBytes = byteData.buffer.asUint8List();
+      ui.Codec codec = await ui.instantiateImageCodec(pngBytes,
+          targetHeight: 900, targetWidth: 2000);
+      ui.FrameInfo frameInfo = await codec.getNextFrame();
+      ui.Image resizedImage = frameInfo.image;
 
-      // Convert to image from the `image` package
-      img.Image imgData = img.decodeImage(pngBytes)!;
+      // Save the resized image to the gallery
+      Uint8List resizedBytes =
+          (await resizedImage.toByteData(format: ui.ImageByteFormat.png))!
+              .buffer
+              .asUint8List();
+      await ImageGallerySaver.saveImage(resizedBytes);
 
-      // Customize your voucher with customer name and payment info
-      // Use Image.asset for assets in Flutter
-      ByteData assetByteData =
-          await rootBundle.load('assets/images/voucher (5).png');
-      List<int> assetBytes = assetByteData.buffer.asUint8List();
-      img.Image voucherImage = img.decodeImage(Uint8List.fromList(assetBytes))!;
-
-      // Merge the voucher image with customer name and payment info
-      _mergeImages(imgData, voucherImage, 0, 200);
-
-      // Get the document directory using path_provider
-      Directory documentsDirectory = await getApplicationDocumentsDirectory();
-
-      // Define o caminho completo para o arquivo
-      String filePath = '${documentsDirectory.path}/voucher_image.jpg';
-
-      // Salva a imagem no caminho especificado
-      File(filePath).writeAsBytesSync(img.encodeJpg(voucherImage)!);
-
-      // Save the final image to the gallery
-      await ImageGallerySaver.saveFile(filePath);
+      // Exibir a SnackBar após salvar a imagem
+      ScaffoldMessenger.of(context).showSnackBar(
+        SnackBar(
+          backgroundColor: ui.Color.fromARGB(255, 27, 27, 57),
+          content: Text('Imagem salva com sucesso!'),
+          duration: Duration(seconds: 2),
+        ),
+      );
     } else {
       print("Failed to convert image to ByteData");
-    }
-  }
-
-  void _mergeImages(
-      img.Image target, img.Image source, int offsetX, int offsetY) {
-    for (int y = 0; y < source.height; y++) {
-      for (int x = 0; x < source.width; x++) {
-        img.Color sourcePixel = source.getPixel(x, y);
-
-        // Define o novo Pixel na imagem de destino
-        target.setPixel(x + offsetX, y + offsetY, sourcePixel);
-      }
     }
   }
 }
